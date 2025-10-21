9243752f89b2a6030593b120593673056046c3bc
emon ahmed rana <emon9996a@gmail.com>
2025-10-22 02:25:27 +0600

    cost calculate page complite

--- script.js
+++ script.js
@@ -1,3 +1,49 @@
+// scroll nav
+
+let lastScrollY = window.scrollY;
+const navbar = document.getElementById('scrollNavbar');
+
+window.addEventListener('scroll', () => {
+  
+    const currentScrollY = window.scrollY;
+
+   
+    if (currentScrollY > lastScrollY) {
+      
+        navbar.style.top = '-100px'; 
+    } 
+   
+    else {
+     
+        navbar.style.top = '0'; 
+    }
+
+  
+    lastScrollY = currentScrollY;
+});
+
+
+
+// -------scroll nav end --------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 document.querySelectorAll('.faq-toggle').forEach(btn=>{
     btn.addEventListener('click',()=>{
       const content = btn.parentElement.nextElementSibling;
